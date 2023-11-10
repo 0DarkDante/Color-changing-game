62b94b77de33867dfd53f8dcb86044862540e422
Dark Dante <92054470+0DarkDante@users.noreply.github.com>
2023-11-10 16:05:47 +0200

    Changing colors in a circle when pressed

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ field = document.getElementById('field');
 let rows = 3;
 let cols = 3;
 
+let colors = ['red', 'green', 'blue'];
+let currentIndex = 0;
+
 for(let i = 0; i < rows; i++) {
   let tr = document.createElement('tr');
   field.appendChild(tr);
@@ -10,6 +13,22 @@ for(let i = 0; i < rows; i++) {
   for(let j = 0; j < cols; j++) {    
     let td = document.createElement('td');
     tr.appendChild(td);  
+    changingСolors(td);
+    nextColors(td);
   }
 }
 
+function changingСolors(td) {
+  let min = 0;
+  let max = 2;
+  let randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+  td.classList.add(colors[randomNum]);
+}
+
+function nextColors(td) {
+  td.addEventListener('click', function(){
+    td.classList.remove(colors[currentIndex]);
+    currentIndex = (currentIndex + 1) % colors.length;
+    td.classList.add(colors[currentIndex]);
+  }); 
+}
