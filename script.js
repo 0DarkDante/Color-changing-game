18456f1fb6c4fde5b8414a936116c9b0d1af3d0b
Dark Dante <92054470+0DarkDante@users.noreply.github.com>
2023-11-10 17:01:07 +0200

    Победа в игре

--- script.js
+++ script.js
@@ -1,34 +1,50 @@
-field = document.getElementById('field');
-
+let field = document.getElementById('field');
 let rows = 3;
 let cols = 3;
-
 let colors = ['red', 'green', 'blue'];
 let currentIndex = 0;
 
-for(let i = 0; i < rows; i++) {
+for (let i = 0; i < rows; i++) {
   let tr = document.createElement('tr');
   field.appendChild(tr);
 
-  for(let j = 0; j < cols; j++) {    
+  for (let j = 0; j < cols; j++) {
     let td = document.createElement('td');
-    tr.appendChild(td);  
-    changingСolors(td);
+    tr.appendChild(td);
+    changingColors(td);
     nextColors(td);
   }
 }
 
-function changingСolors(td) {
+let tds = document.querySelectorAll('td');
+
+function changingColors(td) {
   let min = 0;
   let max = 2;
   let randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
   td.classList.add(colors[randomNum]);
 }
 
+function checkVictory() {
+  let firstTdClass = tds[0].classList[0];
+
+  for (let td of tds) {
+    if (td.classList[0] !== firstTdClass) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
 function nextColors(td) {
-  td.addEventListener('click', function(){
+  td.addEventListener('click', function () {
     td.classList.remove(colors[currentIndex]);
     currentIndex = (currentIndex + 1) % colors.length;
     td.classList.add(colors[currentIndex]);
-  }); 
+
+    if (checkVictory()) {
+      alert('Перемога, вітаю!');
+    }
+  });
 }
